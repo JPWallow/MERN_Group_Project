df7040e99d6239d766d31032a7e592316ba98891
“Dieu <dieuhale@gmail.com>
2022-10-16 16:50:08 -0700

    Update delete button

--- Travel_Project/client/src/components/Dashboard.js
+++ Travel_Project/client/src/components/Dashboard.js
@@ -1,11 +1,11 @@
 import React, {useEffect, useState} from 'react'
 import axios from 'axios'
-import { useNavigate, Link } from 'react-router-dom';
+import {  useNavigate, Link } from 'react-router-dom';
 import '../App.css';
 
 const Dashboard = (props) => {
 
-    const { destinationList, setDestinationList } = props;
+    const { removeFromDom, destinationList, setDestinationList } = props;
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -18,20 +18,27 @@ const Dashboard = (props) => {
             .catch((error) => {
                 console.log(error)
             })
-        
-        
     }, [])
+
+    const deleteDestination = (destinationId) => {
+        axios.delete(`http://localhost:8000/api/travel/${destinationId}` )
+            .then (res => {
+                const newDestinationList = destinationList.filter((oneDestination) => oneDestination._id !== destinationId);
+                setDestinationList(newDestinationList);
+            })
+            .catch(err => console.log(err));
+    }
 
     return (
         <div>
             <div>
                 {/* Will update to show user name once login is working */}
                  {/* Will update logout link once Login/Reg is funtional */}
-                <p>Welcome, User!</p>
-                <Link to="/community">Community</Link> |
                 <Link to="/destinations/new">Add Your Destination</Link> |
+                <Link to="/community">Community</Link> |
                 <Link to="/">Logout</Link>
             </div>
+            <h1>Welcome, User!</h1>
             <div>
                 <table>
                     <thead> 
@@ -47,7 +54,11 @@ const Dashboard = (props) => {
                                 <td>{destination.departed}</td>
                                 <td>{destination.returned}</td>
                                 {/* Will update with correct links */}
-                                <td><Link to={`/destinations/${destination._id}`}>Details</Link> <Link to={`/destinations/edit/${destination._id}`}>Edit</Link> <Link to={``}>Delete</Link></td>
+                                <td>
+                                    <Link to={`/destinations/${destination._id}`}>Details</Link> 
+                                    <Link to={`/destinations/edit/${destination._id}`}>Edit</Link> 
+                                    <button onClick={(e)=>{deleteDestination(destination._id)}}>Delete</button>
+                                </td>
                             </tr>
                         ))}
                     </tbody>
