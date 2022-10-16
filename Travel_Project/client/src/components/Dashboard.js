0a83350fd7e60cc51bf243d31132186612aaef6f
“Dieu <dieuhale@gmail.com>
2022-10-16 16:10:33 -0700

    Update delete button

--- Travel_Project/client/src/components/Dashboard.js
+++ Travel_Project/client/src/components/Dashboard.js
@@ -1,64 +1,60 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { useNavigate, Link } from "react-router-dom";
-import "../App.css";
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+import { useNavigate, Link } from 'react-router-dom';
+import '../App.css';
 
 const Dashboard = (props) => {
-  const { destinationList, setDestinationList } = props;
-  const navigate = useNavigate();
 
-  useEffect(() => {
-    axios
-      .get("http://localhost:8000/api/travel")
-      .then((res) => {
-        console.log(res);
-        console.log(res.data);
-        setDestinationList(res.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
+    const { destinationList, setDestinationList } = props;
+    const navigate = useNavigate();
 
-  return (
-    <div>
-      <div>
-        {/* Will update to show user name once login is working */}
-        {/* Will update logout link once Login/Reg is funtional */}
-        <p>Welcome, User!</p>
-        <Link to="/community">Community</Link> |
-        <Link to="/destinations/new">Add Your Destination</Link> |
-        <Link to="/">Logout</Link>
-      </div>
-      <div>
-        <table>
-          <thead>
-            <th>Destination</th>
-            <th>Departed</th>
-            <th>Returned</th>
-            <th>Actions</th>
-          </thead>
-          <tbody>
-            {destinationList.map((destination, index) => (
-              <tr key={index}>
-                <td>
-                  {destination.city}, {destination.country}
-                </td>
-                <td>{destination.departed}</td>
-                <td>{destination.returned}</td>
-                {/* Will update with correct links */}
-                <td>
-                  <Link to={`/destinations/${destination._id}`}>Details</Link>{" "}
-                  <Link to={`/destinations/edit/${destination._id}`}>Edit</Link>{" "}
-                  <Link to={``}>Delete</Link>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
-  );
-};
+    useEffect(() => {
+        axios.get("http://localhost:8000/api/travel")
+            .then((res) => {
+                console.log(res);
+                console.log(res.data);
+                setDestinationList(res.data);
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+        
+        
+    }, [])
+
+    return (
+        <div>
+            <div>
+                {/* Will update to show user name once login is working */}
+                 {/* Will update logout link once Login/Reg is funtional */}
+                <p>Welcome, User!</p>
+                <Link to="/community">Community</Link> |
+                <Link to="/destinations/new">Add Your Destination</Link> |
+                <Link to="/">Logout</Link>
+            </div>
+            <div>
+                <table>
+                    <thead> 
+                        <th>Destination</th>
+                        <th>Departed</th>
+                        <th>Returned</th>
+                        <th>Actions</th>
+                    </thead>
+                    <tbody>
+                        {destinationList.map((destination, index) => ( 
+                            <tr key={index}>
+                                <td>{destination.city}, {destination.country}</td>
+                                <td>{destination.departed}</td>
+                                <td>{destination.returned}</td>
+                                {/* Will update with correct links */}
+                                <td><Link to={`/destinations/${destination._id}`}>Details</Link> <Link to={`/destinations/edit/${destination._id}`}>Edit</Link> <Link to={``}>Delete</Link></td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+}
 
 export default Dashboard;
