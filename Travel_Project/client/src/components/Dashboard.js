6f55eca131f3000b616c97c0f24a891c5213cd52
Joshua Wallowitch <JPWALLOW@GMAIL.COM>
2022-10-16 19:42:48 -0400

    test comment

--- Travel_Project/client/src/components/Dashboard.js
+++ Travel_Project/client/src/components/Dashboard.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react'
 import axios from 'axios'
 import {  useNavigate, Link } from 'react-router-dom';
 import '../App.css';
-
+//Hi Dieu!
 const Dashboard = (props) => {
 
     const { removeFromDom, destinationList, setDestinationList } = props;
