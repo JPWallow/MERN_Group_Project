f1d237d190325e016a9d178e4888d65832be2ba7
Edward Griffin <eddieg4512@gmail.com>
2022-10-16 23:22:32 -0400

    Login and Reg backend complete

--- Travel_Project/server/server.js
+++ Travel_Project/server/server.js
@@ -1,18 +1,27 @@
 const express = require('express')
 const app = express()
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 const port = 8000
 
 const cors = require('cors')
 app.use(cors({
     origin: "http://localhost:3000"
 }));
+app.use(cors({ credentials: true, origin: process.env.CLIENT_URL }));
+
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
 
 // config
-require("./config/mongoose.config")
+require("./config/mongoose.config");
+
+// .env
+require("dotenv").config();
 
 // routes
 require("./routes/travel.routes")(app)
+require("./routes/user.routes")(app);
 
 
-app.listen(port,()=>{console.log(`Successfully listening and connected to port ${port}`)})
+app.listen(process.env.PORT,()=>{console.log(`Successfully listening and connected to port ${port}`)})
