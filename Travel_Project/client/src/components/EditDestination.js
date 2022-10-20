9227f15dee8a948d9e5070c702d2eea9e6e92b5a
Joshua Wallowitch <JPWALLOW@GMAIL.COM>
2022-10-20 18:46:28 -0400

    edit page turned to text image input

--- Travel_Project/client/src/components/EditDestination.js
+++ Travel_Project/client/src/components/EditDestination.js
@@ -160,8 +160,8 @@ const EditDestination = (props) => {
                             <li>
                                 <label>Upload a picture:</label><br />
                                 <input
-                                    type="file"
-                                    value={""}
+                                    type="text"
+                                    value={boxArt}
                                     name="boxArt"
                                     id="image-input"
                                     accept="image/jpeg, image/png, image/jpg"
